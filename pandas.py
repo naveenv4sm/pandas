2e8ee0141abef0f0f44e2c5e76046f4229747648
naveen <138721464+naveenv4sm@users.noreply.github.com>
2025-02-27 23:19:54 -0500

    Delete pandas.py

--- pandas.py
+++
@@ -1,7 +0,0 @@
-# import the pandas library
-import pandas as pd
-# Dictionary of key pair values called data
-data = {'Name':['Ashika', 'Tanu', 'Ashwin', 'Mohit', 'Sourabh'],
-        'Age': [24, 23, 22, 19, 10]}
-data
-
